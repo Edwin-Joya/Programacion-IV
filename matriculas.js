ce8013d349b02737ddeb370698d2491a9d069538
Edwin-Joya <edwinjoya35@gmail.com>
2023-03-02 20:00:24 -0600

    Agregado matriculas.

--- matriculas.js
+++ matriculas.js
@@ -1,123 +1,99 @@
+Vue.component('v-select-alumnos', VueSelect.VueSelect);
 Vue.component('component-matriculas',{
     data() {
         return {
             accion:'nuevo',
             buscar: '',
             matriculas: [],
+            alumnos : [],
             matricula:{
-                idInscripcion : '',
-                codigo : '',
-                nombre : '',
-                facultad:'',
-                carrera: '',
-            },
-            facultades: [
-                'Ciencias y tecnologia',
-                'Ciencias de la salud',
-            ],
-            carrerasPorFacultad: {
-                'Ciencias y tecnologia':['Tec. Ingeniria en sistemas', 'Ingenieria en sistemas'],
-                'Ciencias de la salud':['Tec. Enfermeria','Licenciatura en Enfermeria'],
-
-
-            },
-            carreras:[],
+                idMatricula  : '',
+                fecha     : '',
+                pago      : false,
+                alumno    : {
+                    id    : '',
+                    label : ''
+                },
+            }
         }
     },
     methods:{
-        guardarInscripcion(){
+        guardarMatricula(){
             this.listar();
             if(this.accion==='nuevo'){
-                this.matricula.idInscripcion = new Date().getTime().toString(16);
+                this.matricula.idMatricula = new Date().getTime().toString(16);
                 this.matriculas.push( JSON.parse( JSON.stringify(this.matricula) ) );
             }else if(this.accion==='modificar'){
-                let index = this.matriculas.findIndex(matricula=>matricula.idInscripcion==this.matricula.idInscripcion);
+                let index = this.matriculas.findIndex(matricula=>matricula.idMatricula==this.matricula.idMatricula);
                 this.matriculas[index] = JSON.parse( JSON.stringify(this.matricula) );
             }else if(this.accion==='eliminar'){
-                let index = this.matriculas.findIndex(matricula=>matricula.idInscripcion==this.matricula.idInscripcion);
+                let index = this.matriculas.findIndex(matricula=>matricula.idMatricula==this.matricula.idMatricula);
                 this.matriculas.splice(index,1);
             }
             localStorage.setItem("matriculas", JSON.stringify(this.matriculas) );
-            this.nuevoInscripcion();
+            this.nuevoMatricula();
         },
-        eliminarInscripcion(matricula){
-            if( confirm(`Esta seguro de eliminar a ${matricula.nombre}?`) ){
+        eliminarMatricula(matricula){
+            if( confirm(`Esta seguro de eliminar a ${matricula.alumno.label}?`) ){
                 this.accion='eliminar';
                 this.matricula=matricula;
-                this.guardarInscripcion();
+                this.guardarMatricula();
             }
         },
-        nuevoInscripcion(){
+        nuevoMatricula(){
             this.accion = 'nuevo';
-            this.matricula.idInscripcion = '';
-            this.matricula.codigo = '';
-            this.matricula.nombre = '';
-            this.matricula.facultad = '';
-            this.matricula.carrera = '';
+            this.matricula.idMatricula = '';
+            this.matricula.fecha = '';
+            this.matricula.pago = false;
+            this.matricula.alumno.id = '';
+            this.matricula.alumno.label = '';
         },
-        modificarInscripcion(matricula){
+        modificarMatricula(matricula){
             this.accion = 'modificar';
             this.matricula = matricula;
         },
         listar(){
             this.matriculas = JSON.parse( localStorage.getItem('matriculas') || "[]" )
-                .filter(matricula=>matricula.nombre.toLowerCase().indexOf(this.buscar.toLowerCase())>-1);
-        },
-        actualizarCarreras() {
-            const carreras = this.carrerasPorFacultad[this.matricula.facultad] || [];
-            this.carreras = carreras;
-            this.matricula.carrera = '';
+                .filter(matricula=>matricula.alumno.label.toLowerCase().indexOf(this.buscar.toLowerCase())>-1 ||
+                    matricula.fecha.indexOf(this.buscar)>-1);
+            this.alumnos = JSON.parse( localStorage.getItem('alumnos') || "[]" ).map(alumno=>{
+                return { 
+                    id: alumno.idAlumno,
+                    label : alumno.nombre
+                }
+            });
         }
     },
     template: `
         <div class="row">
             <div class="col-12 col-md-6">
                 <div class="card">
-                    <div class="card-header">REGISTRO DE INSCRIPCION</div>
+                    <div class="card-header">REGISTRO DE MATRICULA</div>
                     <div class="card-body">
-                        <form id="frmInscripcion" @reset.prevent="nuevoInscripcion" v-on:submit.prevent="guardarInscripcion">
+                        <form id="frmMatricula" @reset.prevent="nuevoMatricula" v-on:submit.prevent="guardarMatricula">
                             <div class="row p-1">
                                 <div class="col-3 col-md-2">
-                                    <label for="txtCodigoInscripcion">CODIGO:</label>
+                                    <label for="txtCodigoMatricula">ALUMNO:</label>
                                 </div>
-                                <div class="col-3 col-md-3">
-                                    <input required pattern="[0-9]{3}" 
-                                        title="Ingrese un codigo de matricula de 3 digitos"
-                                            v-model="matricula.codigo" type="text" class="form-control" name="txtCodigoInscripcion" id="txtCodigoInscripcion">
+                                <div class="col-3 col-md-6">
+                                    <v-select-alumnos required v-model="matricula.alumno" :options="alumnos" ></v-select-alumnos>
                                 </div>
                             </div>
                             <div class="row p-1">
                                 <div class="col-3 col-md-2">
-                                    <label for="txtNombreInscripcion">NOMBRE:</label>
+                                    <label>FECHA:</label>
                                 </div>
-                        
-                            
-                            <div class="col-3 col-md-5">
-                            <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Buscar" aria-label="Buscar" aria-describedby="button-addon2">
-                            <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="bi bi-search"></i></button>
+                                <div class="col-9 col-md-3">
+                                    <input required v-model="matricula.fecha" type="date" class="form-control">
+                                </div>
                             </div>
-                            </div>
-                            </div>
-
-                            <div class="col-3 md-3">
-                            <select v-model="matricula.facultad" class="form-control" id="inputFacultadMatricula"
-                                @change="actualizarCarreras">
-                                <option disabled value="">-</option>
-                                <option v-for="facultad in facultades" :value="facultad">{{facultad}}
-                                </option>
-                            </select>
-                            <div class="col-2 col-md-1">
-                                <label for="txtNombreAlumno">CARRERA:</label>
-                            </div>
-    
-                            <select v-model="matricula.carrera" class="form-control" id="inputCarreraMatricula">
-                                <option disabled value="">-</option>
-                                <option v-for="carrera in carreras" :value="carrera">{{carrera}}</option>
-                            </select>
-                        </div>
-    
-
+                            <div class="row p-1">
+                                <div class="col-3 col-md-2">
+                                    <label for="txtPagoMatricula">ACTUALIZAR PAGO:</label>
+                                </div>
+                                <div class="col-9 col-md-3">
+                                    <input v-model="matricula.pago" type="checkbox" class="form-check-input" id="txtPagoMatricula">
+                                </div>
                             </div>
                             <div class="row p-1">
                                 <div class="col-3 col-md-3">
@@ -134,28 +110,28 @@ Vue.component('component-matriculas',{
             </div>
             <div class="col-12 col-md-6">
                 <div class="card">
-                    <div class="card-header">LISTADO DE INSCRIPCIONS</div>
+                    <div class="card-header">LISTADO DE MATRICULAS</div>
                     <div class="card-body">
                         <table class="table table-bordered table-hover">
                             <thead>
                                 <tr>
                                     <th>BUSCAR:</th>
-                                    <th colspan="2"><input type="text" class="form-control" v-model="buscar"
+                                    <th colspan="3"><input type="text" class="form-control" v-model="buscar"
                                         @keyup="listar()"
                                         placeholder="Buscar por codigo o nombre"></th>
                                 </tr>
                                 <tr>
-                                    <th>CODIGO</th>
-                                    <th colspan="2">NOMBRE</th>
+                                    <th>FECHA</th>
+                                    <th>PAGO</th>
+                                    <th colspan="2">ALUMNO</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr v-for="matricula in matriculas" :key="matricula.idInscripcion" @click="modificarInscripcion(matricula)" >
-                                    <td>{{ matricula.codigo }}</td>
-                                    <td>{{ matricula.nombre }}</td>
-                                    <td>{{ matricula.facultad }}</td>
-                                    <td>{{ matricula.carrera }}</td>
-                                    <td><button class="btn btn-danger" @click="eliminarInscripcion(matricula)">ELIMINAR</button></td>
+                                <tr v-for="matricula in matriculas" :key="matricula.idMatricula" @click="modificarMatricula(matricula)" >
+                                    <td>{{ new Date(matricula.fecha +' 01:00:00').toLocaleDateString() }}</td>
+                                    <td>{{ matricula.pago ? 'ACTUALIZADO' : 'PENDIENTE' }}</td>
+                                    <td>{{ matricula.alumno.label }}</td>
+                                    <td><button class="btn btn-danger" @click="eliminarMatricula(matricula)">ELIMINAR</button></td>
                                 </tr>
                             </tbody>
                         </table>
